62c730c19a20cc23cf180fe75f0413948278f8bf
NiettoVale <nietto.program@gmail.com>
2023-09-05 12:45:14 -0300

    user

--- Server/src/routes/Users_Routes/getUserById.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const getUserById = require("../../controllers/User/getUserById");
-const router = express.Router();
-
-router.get("/profile/:id", getUserById);
-
-module.exports = router;
