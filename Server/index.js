a4b84c28de36abfbb5da0216ca3555a4ef568a73
NiettoVale <nietto.program@gmail.com>
2023-09-13 15:39:07 -0300

    asfdadgfsdadasdasdasdasd

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const server = require("./src/app");
 const { PORT } = process.env || 3001;
-const { connect } = require("./src/database");
+const { connect } = require("./src/DataBase");
 
 connect.sync({ force: false }).then(() => {
   console.log("Conexión con la base de datos exitosa");
