707081938fee3426ae43e475f862e66d427632cc
BenjaPalazzo <benjapalazzo@gmail.com>
2023-09-20 12:31:13 -0300

    Ger te amo

--- Server/src/controllers/Products/postStockProduct.js
+++ Server/src/controllers/Products/postStockProduct.js
@@ -1,23 +1,26 @@
 const { Product, Size, Stock } = require("../../DataBase");
 
 const postStockProduct = async (req, res) => {
-  const { productId, sizeId, change } = req.params;
-
+  const { ProductId, SizeId, change } = req.params;
+  console.log(SizeId);
   try {
     // Buscar el registro de Stock correspondiente al productId y sizeId
-    const stockRecord = await Stock.findOne({ productId, sizeId });
+    const stockRecord = await Stock.findOne({ where: { ProductId, SizeId } });
 
     if (!stockRecord) {
       return res
         .status(404)
         .json({ error: "No se encontró el registro de Stock" });
     }
-
+    console.log(stockRecord.SizeId);
     // Verificar el valor de 'change' y ajustar la cantidad en consecuencia
     if (change === "true") {
       // Aumentar la cantidad en 1 si 'change' es true
       stockRecord.quantity += 1;
       console.log("Cuantiti", stockRecord.quantity);
+      if (stockRecord.changed()) {
+        await stockRecord.save();
+      }
       return res
         .status(200)
         .json({ message: "Stock Aumentado", cantidad: stockRecord.quantity });
@@ -25,6 +28,9 @@ const postStockProduct = async (req, res) => {
       // Disminuir la cantidad en 1 si 'change' es false, pero no permitir cantidades negativas
       if (stockRecord.quantity > 0) {
         stockRecord.quantity -= 1;
+        if (stockRecord.changed()) {
+          await stockRecord.save();
+        }
         return res.status(200).json({
           message: "Stock  Disminuido",
           cantidad: stockRecord.quantity,
--- Server/src/routes/Products_Routes/postStockProduct.routes.js
+++ Server/src/routes/Products_Routes/postStockProduct.routes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const postStockProduct = require("../../controllers/Products/postStockProduct");
 const router = express.Router();
 
-router.post("/products/:productId/:sizeId/:change", postStockProduct);
+router.post("/products/:ProductId/:SizeId/:change", postStockProduct);
 
 module.exports = router;
