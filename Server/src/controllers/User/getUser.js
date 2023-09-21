3b076cfa06e7f89b702f0247de98c007e20d92fb
Valentín Francisco Diaz Nieto <130608630+NiettoVale@users.noreply.github.com>
2023-09-21 06:33:46 -0300

    Update getUser.js

--- Server/src/controllers/User/getUser.js
+++ Server/src/controllers/User/getUser.js
@@ -10,9 +10,7 @@ const getUsers = async (_req, res) => {
       // attributes: ["name"], // Aquí especifica los campos que deseas incluir
     });
 
-    const sliceUsers = users.slice(0, 51);
-
-    return res.status(200).json(sliceUsers);
+    return res.status(200).json(users);
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
